c1d4f555bb7f4e67a5140f1d4edc47719cc63c5f
QueenieJK <jagodaanakurowska@gmail.com>
2025-11-18 23:47:00 +0100

    Dodaj pliki projektów.

---
+++ Lab 6/Lab 6.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+//Zadanie 1:
+int a = 20;
+int b = 10;
+int suma(int liczba, int liczba2);
+int suma(int liczba, int liczba2) {
+	return liczba + liczba2;
+}
+int main()
+{
+	int a = 1;
+
+	
+}
